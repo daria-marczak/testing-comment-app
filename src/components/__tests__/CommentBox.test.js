d625b08baad21061a94ed7cbd1c4ebc582623b3c
daria marczak <marczak.ria@gmail.com>
2018-06-19 20:40:18 +0200

    Did tests for comment box

---
+++ src/components/__tests__/CommentBox.test.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+
+class CommentBox extends Component {
+  state = {
+    comment: ""
+  };
+
+  handleChange = event => {
+    this.setState({ comment: event.target.value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.setState({ comment: "" });
+
+    // TODO - call an action creator
+    // and save a comment
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h4>Add a comment</h4>
+        <textarea value={this.state.comment} onChange={this.handleChange} />
+        <div>
+          <button>Submit comment</button>
+        </div>
+      </form>
+    );
+  }
+}
+
+export default CommentBox;
