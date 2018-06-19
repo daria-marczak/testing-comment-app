22cc346d9420ecb13aa0ba415535fda86f4a0383
daria marczak <marczak.ria@gmail.com>
2018-06-19 20:41:08 +0200

    Did tests for comment box

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,12 +1,26 @@
 import React from "react";
-import CommentList from "components/CommentList";
-import CommentBox from "components/CommentBox";
+import { shallow } from "enzyme";
+// Renders an instance of App component and none of its children
 
-export default () => {
-  return (
-    <div>
-      <CommentBox />
-      <CommentList />
-    </div>
-  );
-};
+import App from "components/App";
+import CommentBox from "components/CommentBox";
+import CommentList from "components/CommentList";
+// I can now move piece files
+
+let wrapped; // It's out of beforeEach scope and can be
+// accessed in those tests
+
+beforeEach(() => {
+  wrapped = shallow(<App />); // Wrapped version of App component
+}); // We extract it so as not to repeat the same line of code
+// before every single line of code, run first this one
+
+it("shows the CommentBox", () => {
+  expect(wrapped.find(CommentBox).length).toEqual(1);
+  // We search for every copy of CommentBox, this gives us an array
+  // and we want it to be exactly 1
+});
+
+it("shows the CommentList", () => {
+  expect(wrapped.find(CommentList).length).toEqual(1);
+});
