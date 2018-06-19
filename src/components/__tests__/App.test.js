a7f87028ef755185f76bdd441ee87e223627b447
daria marczak <marczak.ria@gmail.com>
2018-06-19 20:39:37 +0200

    Refactored imports to be absolute

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,21 +1,12 @@
 import React from "react";
-import { shallow } from "enzyme";
-// renders an instance of App component and none of its children
+import CommentList from "components/CommentList";
+import CommentBox from "components/CommentBox";
 
-import App from "../App";
-import CommentBox from "../CommentBox";
-import CommentList from "../CommentList";
-
-it("shows the CommentBox", () => {
-  const wrapped = shallow(<App />); // wrapped version of App component
-
-  expect(wrapped.find(CommentBox).length).toEqual(1);
-  // we search for every copy of CommentBox, this gives us an array
-  // and we want it to be exactly 1
-});
-
-it("shows the CommentList", () => {
-  const wrapped = shallow(<App />);
-
-  expect(wrapped.find(CommentList).length).toEqual(1);
-});
+export default () => {
+  return (
+    <div>
+      <CommentBox />
+      <CommentList />
+    </div>
+  );
+};
