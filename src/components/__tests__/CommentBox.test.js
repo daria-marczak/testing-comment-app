586e0686866c8d7de82ecfbcb808f5b029e3b111
daria marczak <marczak.ria@gmail.com>
2018-06-20 17:27:06 +0200

    Used describing statement

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -9,45 +9,36 @@ beforeEach(() => {
   wrapped = mount(<CommentBox />);
 });
 
+afterEach(() => {
+  wrapped.unmount();
+});
+
 it("has a text area and a button", () => {
   expect(wrapped.find("button").length).toEqual(1);
   expect(wrapped.find("textarea").length).toEqual(1);
   // Find can be used to find raw HTML elements as well
 });
 
-afterEach(() => {
-  wrapped.unmount();
-});
-// We need to cleanup not to take memory
-
-it("has a text area that users can type in", () => {
-  // Find the textarea element and simulate a change event
-  // We are trying to simulate the HTML event, not the React one
-  wrapped.find("textarea").simulate("change", {
-    target: { value: "New comment" }
+// Describe is used to group together certain sets of tests
+// that have common lines of code
+// It can also limit the scope of beforeEach
+describe("the textarea", () => {
+  beforeEach(() => {
+    wrapped.find("textarea").simulate("change", {
+      target: { value: "New comment" }
+    });
+    wrapped.update();
   });
-  // The second argument is a mock object, to provide a fake event object
-  // This object is merged into real event: it goes into the handleChange
-  // and sets the state to the value
 
-  // Force the component to update as setState is async and we don't want to wait for it
-  wrapped.update();
-  // Assert the textareas value has changed
-
-  expect(wrapped.find("textarea").prop("value")).toEqual("New comment");
-  // The prop is actually the object key in the target
-});
-
-it("has a form that user can submit and is emptied", () => {
-  wrapped.find("textarea").simulate("change", {
-    target: { value: "Something something" }
+  it("has a text area that users can type in", () => {
+    expect(wrapped.find("textarea").prop("value")).toEqual("New comment");
+    // The prop is actually the object key in the target
   });
-  wrapped.update();
-  // First of all we need to simulate the form entering and update
-  // then we can simulate the submit
 
-  wrapped.find("form").simulate("submit");
-  wrapped.update();
+  it("has a form that is emptied after submit", () => {
+    wrapped.find("form").simulate("submit");
+    wrapped.update();
 
-  expect(wrapped.find("textarea").prop("value")).toEqual("");
+    expect(wrapped.find("textarea").prop("value")).toEqual("");
+  });
 });
