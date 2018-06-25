d1153952714eb8426db5eea6a39d965baa2f05b2
daria marczak <marczak.ria@gmail.com>
2018-06-25 19:06:18 +0200

    Added a higher order component for authentication

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,26 +1,13 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 
+import requireAuth from "components/requireAuth";
 import * as actions from "actions";
 
 class CommentBox extends Component {
   state = {
     comment: ""
   };
-
-  componentDidMount() {
-    this.shouldNavigateAway();
-  }
-
-  componentDidUpdate() {
-    this.shouldNavigateAway();
-  }
-
-  shouldNavigateAway() {
-    if (!this.props.auth) {
-      this.props.history.push("/");
-    }
-  }
 
   handleChange = event => {
     this.setState({ comment: event.target.value });
@@ -50,10 +37,6 @@ class CommentBox extends Component {
   }
 }
 
-function mapStateToProps(state) {
-  return { auth: state.auth };
-}
-
 // CommentBox does not need the access to all of the state
 // connect expects to see some parent component that has the provider tag
-export default connect(mapStateToProps, actions)(CommentBox);
+export default connect(null, actions)(requireAuth(CommentBox));
