1e2e362417f9ecd45b26978f8a8fac3a04f1ad45
daria marczak <marczak.ria@gmail.com>
2018-06-25 18:54:44 +0200

    Added navigation for the app

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,45 @@
-import React from "react";
-import { Route } from "react-router-dom";
+import React, { Component } from "react";
+import { Route, Link } from "react-router-dom";
 import CommentList from "components/CommentList";
 import CommentBox from "components/CommentBox";
+import { connect } from "react-redux";
 
-export default () => {
-  return (
-    <div>
-      <Route path="/post" component={CommentBox} />
-      <Route exact path="/" component={CommentList} />
-    </div>
-  );
-};
+class App extends Component {
+  renderButton() {
+    if (this.props.auth) {
+      return <button>Sign out</button>;
+    } else {
+      return <button>Sign in</button>;
+    }
+  }
+
+  renderHeader() {
+    return (
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/post">Post a comment</Link>
+        </li>
+        <li>{this.renderButton()}</li>
+      </ul>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        {this.renderHeader()}
+        <Route path="/post" component={CommentBox} />
+        <Route exact path="/" component={CommentList} />
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return { auth: state.auth };
+}
+
+export default connect(mapStateToProps)(App);
