0a37a3f664d3ea698df194d7137a5f4c32c52b2d
daria marczak <marczak.ria@gmail.com>
2018-06-27 20:55:55 +0200

    Added emitting errors for validation

--- src/Root.js
+++ src/Root.js
@@ -1,25 +1,25 @@
 import React from "react";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
-import reducers from "reducers";
 import async from "middlewares/async";
+import stateValidator from "middlewares/stateValidator";
+import reducers from "reducers";
 
 export default ({ children, initialState = {} }) => {
   const store = createStore(
     reducers,
     initialState,
-    applyMiddleware(async)
+    applyMiddleware(async, stateValidator)
   );
 
   return (
+    // Provider is a React component that communicates directly
+    // with every connected component
+
+    // The empty object is for the store that is not created
     <Provider store={store}>{children}</Provider>
   );
 };
-
-// Provider is a React component that communicates directly
-// with every connected component
-
-// The empty object is for the store that is not created
 
 // Props.children is a React construct, allows to take the component
 // we've created and to wrap other components
